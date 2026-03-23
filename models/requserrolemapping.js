f65cd8a15e4464d0f36e70fb567d2254ddf981cb
Vaisakhgtechversant <vaisakhg@techversantinfotech.com>
2026-03-23 10:20:37 +0530

    Add requserrolemapping model

---
+++ models/requserrolemapping.js
@@ -0,0 +1,32 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class reqUserRoleMapping extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  reqUserRoleMapping.init({
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    userId: DataTypes.INTEGER,
+    roleId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'reqUserRoleMapping',
+    tableName: 'reqUserRoleMapping',
+    timestamps: false
+  });
+  return reqUserRoleMapping;
+};
