d75c638d908c8d907395e638ff4633f69785b552
Vaisakhgtechversant <vaisakhg@techversantinfotech.com>
2026-08-04 17:10:56 +0530

    remove hold apis

--- api/routes/users.route.js
+++ api/routes/users.route.js
@@ -40,10 +40,6 @@ router.get('/preffer-location', authenticate, commonController.prefferedList);
 
 router.post('/edit-progress',authenticate,commonController.editProgressV1);
 
-router.post('/hold', authenticate, commonController.holdProgress);
-
-router.get('/hold/comments', authenticate, commonController.holdComments);
-
 router.post('/google-login', authController.googleLogin)
 
 
