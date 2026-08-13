1fb430dcc6f946024a0eeaf32cda8db399af80bd
Vaisakhgtechversant <vaisakhg@techversantinfotech.com>
2026-08-13 15:09:11 +0530

    add refresh token

--- api/utils/jwt.js
+++ api/utils/jwt.js
@@ -5,26 +5,26 @@ const jwt = require("jsonwebtoken");
 let secret = 'xxx';
 
 // Generate short-lived access token
-// exports.jwtToken = async (userObj) => {
-//     const token = jwt.sign(userObj, dotenv.TOKEN_SECRET, {
-//         expiresIn: dotenv.TOKEN_EXPIRY || '1h'
-//     });
-//     return token;
-// }
 exports.jwtToken = async (userObj) => {
-    const token = jwt.sign(userObj, process.env.TOKEN_SECRET);
+    const token = jwt.sign(userObj, process.env.TOKEN_SECRET, {
+        expiresIn: process.env.TOKEN_EXPIRY || '1h'
+    });
     return token;
 }
+// exports.jwtToken = async (userObj) => {
+//     const token = jwt.sign(userObj, process.env.TOKEN_SECRET);
+//     return token;
+// }
 
 // Generate long-lived refresh token
-// exports.jwtRefreshToken = async (userObj) => {
-//     const refreshToken = jwt.sign(
-//         { userId: userObj.userId },
-//         dotenv.REFRESH_TOKEN_SECRET,
-//         { expiresIn: dotenv.REFRESH_TOKEN_EXPIRY || '1h' }
-//     );
-//     return refreshToken;
-// }
+exports.jwtRefreshToken = async (userObj) => {
+    const refreshToken = jwt.sign(
+        { userId: userObj.userId },
+        process.env.REFRESH_TOKEN_SECRET,
+        { expiresIn: process.env.REFRESH_TOKEN_EXPIRY || '1h' }
+    );
+    return refreshToken;
+}
 
 // Verify access token
 exports.jwtVerifyToken = async (token) => {
@@ -51,23 +51,23 @@ exports.jwtVerifyToken = async (token) => {
 }
 
 // Verify refresh token
-// exports.jwtVerifyRefreshToken = async (token) => {
-//     try {
-//         const verified = jwt.verify(token, dotenv.REFRESH_TOKEN_SECRET || dotenv.TOKEN_SECRET);
-//         return verified;
-//     } catch (err) {
-//         if (err.name === 'TokenExpiredError') {
-//             const error = new Error('Refresh token expired. Please re-login.');
-//             error.status = 401;
-//             error.code = 'REFRESH_TOKEN_EXPIRED';
-//             throw error;
-//         }
-//         if (err.name === 'JsonWebTokenError') {
-//             const error = new Error('Invalid refresh token.');
-//             error.status = 403;
-//             error.code = 'INVALID_REFRESH_TOKEN';
-//             throw error;
-//         }
-//         throw err;
-//     }
-// }
+exports.jwtVerifyRefreshToken = async (token) => {
+    try {
+        const verified = jwt.verify(token, process.env.REFRESH_TOKEN_SECRET || process.env.TOKEN_SECRET);
+        return verified;
+    } catch (err) {
+        if (err.name === 'TokenExpiredError') {
+            const error = new Error('Refresh token expired. Please re-login.');
+            error.status = 401;
+            error.code = 'REFRESH_TOKEN_EXPIRED';
+            throw error;
+        }
+        if (err.name === 'JsonWebTokenError') {
+            const error = new Error('Invalid refresh token.');
+            error.status = 403;
+            error.code = 'INVALID_REFRESH_TOKEN';
+            throw error;
+        }
+        throw err;
+    }
+}
